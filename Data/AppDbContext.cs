51bc18def36237740fcdc2ae0bd8c4425f10bf97
sonchouz <sofianikonorova41@gmail.com>
2026-01-13 12:33:39 +0700

    set data context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using upserver.Models;
+
+namespace upserver.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+        }
+        public DbSet<Building> Buildings => Set<Building>();
+        public DbSet<Classroom> Classrooms => Set<Classroom>();
+        public DbSet<Teacher> Teachers => Set<Teacher>();
+        public DbSet<Subject> Subjects => Set<Subject>();
+        public DbSet<Specialty> Specialties => Set<Specialty>();
+        public DbSet<StudentGroup> StudentGroups => Set<StudentGroup>();
+        public DbSet<Weekday> Weekdays => Set<Weekday>();
+        public DbSet<LessonTime> LessonTimes => Set<LessonTime>();
+        public DbSet<Schedule> Schedules => Set<Schedule>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Schedule>().HasIndex(s => new { s.LessonDate, s.LessonTimeId, s.GroupId, s.GroupPart }).IsUnique();
+
+            modelBuilder.Entity<Schedule>().HasIndex(s => new { s.LessonDate, s.LessonTimeId, s.ClassroomId }).IsUnique();
+
+            modelBuilder.Entity<Schedule>().Property(s => s.GroupPart).HasConversion<string>();
+        }
+    }
+}
