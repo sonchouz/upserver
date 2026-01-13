ad5e2bedf2320744f92a3a8123279be7506bb325
sonchouz <sofianikonorova41@gmail.com>
2026-01-13 12:57:09 +0700

    turn on connection to database

--- Program.cs
+++ Program.cs
@@ -1,25 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using upserver.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+DotNetEnv.Env.Load();
+
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+$"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+$"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+$"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>;
 
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
-
 app.Run();
