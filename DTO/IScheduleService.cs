00ca1e30776875a2c7c6cbce47e32136d595fdf5
sonchouz <sofianikonorova41@gmail.com>
2026-01-13 13:13:58 +0700

    create interface

---
+++ DTO/IScheduleService.cs
@@ -0,0 +1,7 @@
+﻿namespace upserver.DTO
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup (string groupName, DateTime startDate,  DateTime endDate);
+    }
+}
